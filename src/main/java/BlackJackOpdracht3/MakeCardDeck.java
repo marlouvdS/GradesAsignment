711c26d628e5607c27373832a40a08a5f9dce20f
marlouvdS <44812738+marlouvdS@users.noreply.github.com>
2018-11-24 13:37:09 +0100

    voeg punten toe via switch

--- src/main/java/BlackJackOpdracht3/MakeCardDeck.java
+++ src/main/java/BlackJackOpdracht3/MakeCardDeck.java
@@ -1,6 +1,7 @@
 package BlackJackOpdracht3;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MakeCardDeck extends Card {
@@ -13,23 +14,60 @@ public class MakeCardDeck extends Card {
     static String[] icons = {"♠", "♥", "♣", "♦"};
 
 
-
     static List CardDeck() {
-        List<Card> deck;
-        deck = new ArrayList();
+        List<Card> deck = new ArrayList();
         for (String i : values) {
             for (String j : icons) {
                 String name = i + " of " + j;
-
                 Card card = new Card(i, j);
                 deck.add(card);
             }
         }
+        Collections.shuffle(deck);
         return deck;
     }
 
 
-// voor punten berekenen gebruik cases
+    // voor punten berekenen gebruik cases
+    static int getPoints(Card card) {
+        int points = 0;
+        switch (card.getValue()) {
+            case "A":
+                points = 1;
+                break;
+            case "queen":
+            case "king":
+            case "jack":
+            case "10":
+                points = 10;
+                break;
+            case "9":
+                points = 9;
+                break;
+            case "8":
+                points = 8;
+                break;
+            case "7":
+                points = 7;
+                break;
+            case "6":
+                points = 6;
+                break;
+            case "5":
+                points = 5;
+                break;
+            case "4":
+                points = 4;
+                break;
+            case "3":
+                points = 3;
+                break;
+            case "2":
+                points = 2;
+                break;
+        }
+        return points;
+    }
 
 
 }
